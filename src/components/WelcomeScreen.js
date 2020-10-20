63ffdccca3b3450f13f319125a796921d5cadf12
gitName <a@b.c>
2020-10-20 21:32:08 +0200

    Updated intro text

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -4,7 +4,7 @@ const WelcomeScreen = (props) => {
     return (
         <>
             <div className="app">
-                <p>Start the game when you are ready to test your reaction <span role="img" aria-label="Lightning">⚡️</span></p>
+                <p>Start the game to test your reaction time</p>
                 <input type="button" value="Start game" onClick={props.startGame} />
             </div>
         </>
