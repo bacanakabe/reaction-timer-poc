5a9865282250d99adae03d23ad54b459fe911111
gitName <a@b.c>
2020-10-20 21:29:46 +0200

    Change text

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -4,7 +4,7 @@ const WelcomeScreen = (props) => {
     return (
         <>
             <div className="app">
-                <p><span role="img" aria-label="Lightning">⚡️</span> Start the game when you are ready to test your reaction <span role="img" aria-label="Lightning">⚡️</span></p>
+                <p>Start the game when you are ready to test your reaction <span role="img" aria-label="Lightning">⚡️</span></p>
                 <input type="button" value="Start game" onClick={props.startGame} />
             </div>
         </>
